a5a88450d2aa41a37c5be9059a090caa971bca2a
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-08-19 14:11:28 -0500

    Expose random seed and connectivity helpers

--- bzr_heightmap/__init__.py
+++ bzr_heightmap/__init__.py
@@ -1,4 +1,4 @@
-from .analysis import describe_heightmap, make_preview, terrain_metrics
+from .analysis import describe_heightmap, make_preview, terrain_metrics, traversability_metrics
 from .hg2 import (
     BZ_ZONE_WORLD_SIZE,
     DEFAULT_ZONE_BITS,
@@ -11,12 +11,13 @@ from .hg2 import (
     HG2Map,
 )
 from .recipes import RECIPES, generate
-from .settings import GeneratorSettings
+from .settings import GeneratorSettings, RANDOM_SEED_MAX, random_seed, resolve_seed
 
 __all__ = [
     "BZ_ZONE_WORLD_SIZE",
     "DEFAULT_ZONE_BITS",
     "GeneratorSettings",
+    "RANDOM_SEED_MAX",
     "HG2_HEIGHT_MASK",
     "HG2_MAP_VERSION",
     "HG2_MAX_HEIGHT",
@@ -28,5 +29,8 @@ __all__ = [
     "describe_heightmap",
     "generate",
     "make_preview",
+    "random_seed",
+    "resolve_seed",
     "terrain_metrics",
+    "traversability_metrics",
 ]
